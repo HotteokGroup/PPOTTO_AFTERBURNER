9f1b3dd75f3c455296516e6b8f6038459221faa0
Seongrok.lee <55491354+argon1025@users.noreply.github.com>
2023-09-11 17:18:54 +0900

    [#3] :sparkles: 이미지 리사이징 및 저장기능 (#5)

---
+++ src/file-process/image-process-interface.ts
@@ -0,0 +1,21 @@
+export interface ResizeImageToWebpOptions {
+  image: Buffer;
+  width: number;
+  height?: number;
+}
+
+export interface GetFileFromS3Options {
+  /** 버킷 이름 */
+  bucket: string;
+  /** 파일 위치 */
+  key: string;
+}
+
+export interface SaveFileToS3Options {
+  /** 버킷 이름 */
+  bucket: string;
+  /** 저장할 위치 */
+  key: string;
+  /** 저장할 파일 */
+  file: Buffer;
+}
